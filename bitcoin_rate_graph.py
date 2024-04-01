363e394844c4d88e1de20675f0a6e408cda6a31f
sv7v <>
2024-04-01 06:02:17 +0300

    IMP возможность переключения в логарифмический масштаб и в линейный

--- bitcoin_rate_graph.py
+++ bitcoin_rate_graph.py
@@ -23,12 +23,23 @@ class BitcoinRateGraph:
 	def _readData(self, s):
 		canvas = BG_HtmlCanvas(640, 480)
 		document <= canvas.get()
-		self._decart = BG_Decart(canvas, (BG_LogY()), BG_Frame())
-		self._decart.draw(BG_TableFunc(BitcoinRateGraph._convertData(s)))
+		self._decart  = BG_Decart(canvas, (BG_LogY()), BG_Frame())
+		self._bitcoin = BG_TableFunc(BitcoinRateGraph._convertData(s))
+		self._decart.draw(self._bitcoin)
+
+	def redraw(self):
+		print(820)
+		self._decart.setProp(BG_LogY() if self._checkbox.getState() else ())
+		self._decart.redraw(self._bitcoin)
 
 	def __init__(self):
 		document <= 'Файл данных: '
 		document <= BG_LocalTextFile(self._readData).get()
+
+		document <= 'Логарифмический масштаб:'
+		self._checkbox = BG_CheckBox(lambda event: self.redraw())
+		document <= self._checkbox.get()
+		self._checkbox.set()
 #class BitcoinRateGraph:
 
 def main():
